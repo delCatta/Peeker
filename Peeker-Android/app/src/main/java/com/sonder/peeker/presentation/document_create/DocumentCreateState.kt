08b51c771928d0032ed0fb23ea7be444b5de33be
Diego Quezada <diego.quezadac@sansano.usm.cl>
2022-05-28 00:02:31 -0400

    add document create form

--- Peeker-Android/app/src/main/java/com/sonder/peeker/presentation/document_create/DocumentCreateState.kt
+++ Peeker-Android/app/src/main/java/com/sonder/peeker/presentation/document_create/DocumentCreateState.kt
@@ -1,5 +1,15 @@
 package com.sonder.peeker.presentation.document_create
 
+import java.time.LocalDate
+import java.util.*
+
 data class DocumentCreateState(
     val documentName: String? = null,
+    val documentDescription: String? = null,
+    val documentType: Int? = null,
+    val documentDateOfIssue: String? = null,
+    val documentExpirationDate: String? = null,
+    val documentTags: List<String>? = null,
+    val error: String? = null,
+    val isLoading: Boolean = false
 )
