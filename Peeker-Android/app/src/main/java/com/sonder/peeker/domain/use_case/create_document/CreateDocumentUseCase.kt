6e38b16d8fd93997422fadb8876ababbe9e72b18
Diego Cattarinich Clavel <dev.cattarinich@gmail.com>
2022-07-04 20:31:34 -0400

    file picker

--- Peeker-Android/app/src/main/java/com/sonder/peeker/domain/use_case/create_document/CreateDocumentUseCase.kt
+++ Peeker-Android/app/src/main/java/com/sonder/peeker/domain/use_case/create_document/CreateDocumentUseCase.kt
@@ -1,19 +1,22 @@
 package com.sonder.peeker.domain.use_case.create_document
 
+import android.util.Log
 import com.sonder.peeker.core.Resource
 import com.sonder.peeker.data.remote.dto.DocumentCreateDto
+import com.sonder.peeker.data.remote.dto.DocumentDto
 import com.sonder.peeker.data.remote.dto.toDocument
 import com.sonder.peeker.domain.model.Document
 import com.sonder.peeker.domain.repository.PeekerRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
+import retrofit2.*
+import java.io.File
 import java.io.IOException
 import javax.inject.Inject
 
 class CreateDocumentUseCase @Inject constructor(
     private val repository: PeekerRepository
-){
+) {
     operator fun invoke(documentCreateDto: DocumentCreateDto): Flow<Resource<Document>> = flow {
         try {
             emit(Resource.Loading<Document>())
@@ -26,6 +29,7 @@ class CreateDocumentUseCase @Inject constructor(
 
         }
     }
+
     fun fromEmptyDocument(): Flow<Resource<Document>> = flow {
         try {
             emit(Resource.Loading<Document>())
@@ -34,6 +38,39 @@ class CreateDocumentUseCase @Inject constructor(
         } catch (e: HttpException) {
             emit(Resource.Error<Document>(e.localizedMessage ?: "An unexpected error ocurred."))
         } catch (e: IOException) {
+            emit(Resource.Error<Document>("Couldn't reach server. Check your internet connection."))
+
+        }
+    }
+
+    fun fromDocumentFile(file: File): Flow<Resource<Document>> = flow {
+        try {
+            emit(Resource.Loading<Document>())
+//            val document = repository.createDocumentWithFile(file)
+//            emit(Resource.Success(document.toDocument()))
+            repository.createDocumentWithFile(file).enqueue(
+                object : Callback<DocumentDto> {
+                    override fun onFailure(call: Call<DocumentDto>, t: Throwable) {
+                        Log.d("Failed", t.localizedMessage)
+
+                    }
+
+                    override fun onResponse(
+                        call: Call<DocumentDto>,
+                        response: Response<DocumentDto>
+                    ) {
+                        Log.d("Response", response.toString())
+
+                    }
+
+                }
+            )
+
+            emit(Resource.Error<Document>("An unexpected error ocurred."))
+        } catch (e: HttpException) {
+            emit(Resource.Error<Document>(e.localizedMessage ?: "An unexpected error ocurred."))
+        } catch (e: IOException) {
+            Log.d("Error", e.localizedMessage)
             emit(Resource.Error<Document>("Couldn't reach server. Check your internet connection."))
 
         }
